6f085d7aeea834ed930b110cf295762a1f7abae0
Thiago Colucci <thiagocolucci@PokeDex.local>
2010-08-30 18:42:15 -0300

    Inicio do corpo da monografia

---
+++ Pagina/Site/TCC_files/TCC.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.1 local-build-20100830
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWPhotoFrame([IWCreateImage('TCC_files/Formal_inset_01.png'),IWCreateImage('TCC_files/Formal_inset_02.png'),IWCreateImage('TCC_files/Formal_inset_03.png'),IWCreateImage('TCC_files/Formal_inset_06.png'),IWCreateImage('TCC_files/Formal_inset_09.png'),IWCreateImage('TCC_files/Formal_inset_08.png'),IWCreateImage('TCC_files/Formal_inset_07.png'),IWCreateImage('TCC_files/Formal_inset_04.png')],null,0,0.800000,1.000000,1.000000,1.000000,1.000000,14.000000,14.000000,14.000000,14.000000,191.000000,262.000000,191.000000,262.000000,null,null,null,0.100000)});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('TCC_files/TCCMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id5');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
